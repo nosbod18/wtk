f5fc1a55e4e20b732d9dce114038b61d72e7ad42
Evan Dobson <evan.dobson18@gmail.com>
2023-01-14 01:26:20 -0600

    Deleted build.sh

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-SRCS="src/wtk.c main.c"
-FLGS="-Wall -Wextra -Wno-deprecated-declarations -std=c99 -x objective-c"
-LIBS="-framework Cocoa -framework OpenGL"
-
-gcc $FLGS $SRCS $LIBS && ./a.out
